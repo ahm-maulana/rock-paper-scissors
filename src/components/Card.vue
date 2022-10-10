33c7c857c68e3ffefcb691a3c38b753c91f7918a
AhmadMaulana <ahmmaulana.alan@gmail.com>
2022-10-11 06:31:58 +0700

    Fix image url

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,13 @@
 <script setup>
 const props = defineProps(['card'])
+const imageUrl = new URL(`/src/assets/images/icon-${props.card.name}.svg`, import.meta.url)
 
 </script>
 
 <template>
     <div class="flex justify-center items-center rounded-full">
         <div class="flex items-center justify-center bg-[#E8E8E8] w-3/4 h-3/4 rounded-full p-4">
-            <img :src="`/src/assets/images/icon-${props.card.name}.svg`" alt="">
+            <img :src="imageUrl" alt="">
         </div>
     </div>
 </template>
